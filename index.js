805f867c3a8fd5f3ca1ae4c5d76ba7c6b68da29a
anilsolanki9 <anilsolanki1192@gmail.com>
2025-01-14 17:15:30 +0530

    enhanced the javaScript code

--- index.js
+++ index.js
@@ -1,81 +1,24 @@
 function throwDice() {
 	var audio = document.getElementById("clickSound");
 	audio.play();
-	// const button = document.getElementById("myButton");
-
-  // const clickAudio = document.getElementById("clickSound");
-  // button.addEventListener("click", () => {
-  //   clickAudio.play();
-	// });
-	
 	var firstValue = Math.random();
 	var secondValue = Math.random();
 	firstValue = firstValue * 6 ;
 	firstValue = Math.floor(firstValue) + 1;
 	secondValue = secondValue * 6 ;
 	secondValue = Math.floor(secondValue) + 1;
-	// document.querySelector("first").setAttribute("src", "./images/dice" + firstValue + ".png");
-	console.log(firstValue);
-	console.log(secondValue);
-
-	// document.querySelector("second").setAttribute("src", "./images/dice" + secondValue + ".png");
 	document.querySelector("input").setAttribute("value", "Throw Again");
-	if (firstValue == 1) {
-		document.querySelector(".first").setAttribute("src", "images/dice1.png");
-		document.querySelector(".playerOne").textContent = "Player 1 (1)";
-	}
-	else if (firstValue == 2) {
-		document.querySelector(".first").setAttribute("src", "images/dice2.png");
-		document.querySelector(".playerOne").textContent = "Player 1 (2)";
-	}
-	else if (firstValue == 3) {
-		document.querySelector(".first").setAttribute("src", "images/dice3.png");
-		document.querySelector(".playerOne").textContent = "Player 1 (3)";
-	}
-	else if (firstValue == 4) {
-		document.querySelector(".first").setAttribute("src", "images/dice4.png");
-		document.querySelector(".playerOne").textContent = "Player 1 (4)";
-	}
-	else if (firstValue == 5) {
-		document.querySelector(".first").setAttribute("src", "images/dice5.png");
-		document.querySelector(".playerOne").textContent = "Player 1 (5)";
-	}
-	else if (firstValue == 6) {
-		document.querySelector(".first").setAttribute("src", "images/dice6.png");
-		document.querySelector(".playerOne").textContent = "Player 1 (6)";
-	}
-
-	if (secondValue == 1) {
-		document.querySelector(".second").setAttribute("src", "images/dice1.png");
-		document.querySelector(".playerTwo").textContent = "Player 2 (1)";
-	}
-	else if (secondValue == 2) {
-		document.querySelector(".second").setAttribute("src", "images/dice2.png");
-		document.querySelector(".playerTwo").textContent = "Player 2 (2)";
-	}
-	else if (secondValue == 3) {
-		document.querySelector(".second").setAttribute("src", "images/dice3.png");
-		document.querySelector(".playerTwo").textContent = "Player 2 (3)";
-	}
-	else if (secondValue == 4) {
-		document.querySelector(".second").setAttribute("src", "images/dice4.png");
-		document.querySelector(".playerTwo").textContent = "Player 2 (4)";
-	}
-	else if (secondValue == 5) {
-		document.querySelector(".second").setAttribute("src", "images/dice5.png");
-		document.querySelector(".playerTwo").textContent = "Player 2 (5)";
-	}
-	else if (secondValue == 6) {
-		document.querySelector(".second").setAttribute("src", "images/dice6.png");
-		document.querySelector(".playerTwo").textContent = "Player 2 (6)";
-	}
+	var randomImage1Source = "./images/dice" + firstValue + ".png";
+	var randomImage2Source = "./images/dice" + secondValue + ".png";
+	document.querySelector(".first").setAttribute("src", randomImage1Source);
+	document.querySelector(".second").setAttribute("src", randomImage2Source);
 	if (firstValue>secondValue) {
 		document.querySelector("h1").textContent = "👍🎉Player 1   Wins👎";
 	}
-	if (firstValue<secondValue) {
+	else if (firstValue<secondValue) {
 		document.querySelector("h1").textContent = "👎Player 2   Wins👍🎉";
 	}
-	if (firstValue==secondValue) {
+	else if (firstValue==secondValue) {
 		document.querySelector("h1").textContent = "🎉 Draw 🎉";
 	}
 }
